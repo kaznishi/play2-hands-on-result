6740dce2ed78e90482c77eed60f988fa2021438e
kaznishi <kaz.nishimu@gmail.com>
2015-10-12 17:42:17 +0900

    implemented remove api

--- app/controllers/JsonController.scala
+++ app/controllers/JsonController.scala
@@ -87,6 +87,11 @@ class JsonController @Inject()(val dbConfigProvider: DatabaseConfigProvider) ext
     /**
      * ユーザ削除
      */
-    def remove(id: Long) = TODO
+    def remove(id: Long) = Action.async { implicit rs =>
+        // ユーザを削除
+        db.run(Users.filter(t => t.id === id.bind).delete).map { _ =>
+            Ok(Json.obj("result" -> "success"))
+        }
+    }
 
 }
