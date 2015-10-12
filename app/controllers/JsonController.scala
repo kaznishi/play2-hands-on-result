ccca1904d8d3631cd07e51825b4ac67f261f36c8
kaznishi <kaz.nishimu@gmail.com>
2015-10-12 17:29:00 +0900

    implemented user/list api

--- app/controllers/JsonController.scala
+++ app/controllers/JsonController.scala
@@ -13,13 +13,33 @@ import slick.driver.H2Driver.api._
 import play.api.libs.json._
 import play.api.libs.functional.syntax._
 
+object JsonController {
+    // ユーザ情報を受取るためのケースクラス
+    case class UserForm(id: Option[Long], name: String, companyId: Option[Int])
+
+    // UsersRowをJSONに変換するためのWritesを定義
+    implicit val usersRowWrites = (
+        (__ \ "id"       ).write[Long]   and
+        (__ \ "name"     ).write[String] and
+        (__ \ "companyId").writeNullable[Int]
+    )(unlift(UsersRow.unapply))
+}
+
 class JsonController @Inject()(val dbConfigProvider: DatabaseConfigProvider) extends Controller
     with HasDatabaseConfigProvider[JdbcProfile] {
+
+    import JsonController._
 
     /**
      * 一覧表示
      */
-    def list = TODO
+    def list = Action.async { implicit rs =>
+        // IDの昇順にすべてのユーザ情報を取得
+        db.run(Users.sortBy(t => t.id).result).map { users =>
+            // ユーザの一覧をJSONで返す
+            Ok(Json.obj("users" -> users))
+        }
+    }
 
     /**
      * ユーザ登録
